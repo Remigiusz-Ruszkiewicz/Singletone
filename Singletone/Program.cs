0e96f451de3fb3b73a17e683af7969e9f86999a5
Remigiusz Ruszkiewicz <r.ruszkiewicz@microsoft.wsei.edu.pl>
2019-10-22 19:30:01 +0200

    Add project files.

---
+++ Singletone/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singletone
+{
+    class Program
+    {
+        static User user = null;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj Imię");
+            var name = Console.ReadLine();
+            Console.WriteLine("Podaj Nazwisko");
+            var lastname = Console.ReadLine();
+            Console.WriteLine("Podaj Hasło");
+            var password = Console.ReadLine();
+            user = User.Instance(name,lastname,password);
+            var newuser = User.Instance("Roman","Nowak","aaa");
+            Console.WriteLine($"{user.Name} {user.LastName}");
+            Console.WriteLine($"{newuser.Name} {newuser.LastName}");
+            if (newuser.HasAcces)
+            {
+                Console.WriteLine("OK");
+            }
+            Console.Read();
+        }
+
+    }
+}
---
+++ Singletone/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singletone
+{
+    public class User
+    {
+        public string Name { get; private set; }
+        public string LastName { get; private set; }
+        private string password;
+        public bool HasAcces { get; private set; } = false;
+        private static User instance;
+        protected User() { }
+        public static User Instance(string name,string lastName,string password)
+        {
+            if (instance == null)
+            {
+                instance = new User();
+                
+                if (password == "abc")
+                {
+                    instance.Name = name;
+                    instance.LastName = lastName;
+                    instance.password = password;
+                    instance.HasAcces = true;
+                }
+            }
+            return instance;
+        }
+    }
+}
